92f55f74df6ea4960a2d36cf4d8697bc7ca21686
Diablopath69 <belionfx69@gmail.com>
2026-06-13 03:00:06 +0700

    fix windUI > main 2

--- loader.lua
+++ loader.lua
@@ -1,2 +1,1 @@
--- Leon X Loader — Wind UI (primary)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/leonx24/Leon-x/main/main-windui.lua?t="..os.time()))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/leonx24/Leon-x/main/main.lua"))()
