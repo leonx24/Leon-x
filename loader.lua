7214b72681d04703c8254bc9a02211e8cfe480fd
Diablopath69 <belionfx69@gmail.com>
2026-06-17 19:13:46 +0700

    fix cant execute dongo

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/leonx24/Leon-x/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/leonx24/Leon-x/main/main.lua"))()
